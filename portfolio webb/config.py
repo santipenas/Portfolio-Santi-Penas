0028020dcef64cbd3b16d2a81b0ec5d7388f492a
Santiago Penas <penassantiago@gmail.com>
2025-01-21 17:41:25 -0300

    new knnn project

--- portfolio webb/config.py
+++
@@ -1,2 +0,0 @@
-class Config:
-    SECRET_KEY = 'tu_clave_secreta'
--- portfolio webb/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
-
-class ContactForm(FlaskForm):
-    name = StringField('Nombre', validators=[DataRequired(), Length(max=50)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    message = TextAreaField('Mensaje', validators=[DataRequired(), Length(max=500)])
-    submit = SubmitField('Enviar')
